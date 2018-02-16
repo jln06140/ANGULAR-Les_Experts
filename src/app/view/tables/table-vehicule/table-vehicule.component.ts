4f14c18ef0e7a3844cf41b32310294556bb5f041
Kedy <gerome.grignon@gmail.com>
2018-02-16 11:57:17 +0100

    table vehicule

--- src/app/view/tables/table-vehicule/table-vehicule.component.ts
+++ src/app/view/tables/table-vehicule/table-vehicule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehicule } from '../../../model/vehicule';
 
 @Component({
   selector: 'app-table-vehicule',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-vehicule.component.css']
 })
 export class TableVehiculeComponent implements OnInit {
-
+  vehiculeColumns = ['date', 'name', 'brand', 'color', 'licensePlate', 'description'];
+  vehiculeSource = generatedListVehicule;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+const generatedListVehicule: Vehicule[] = [
+  {id: 1, date: '02/05/2016', name: 'XS200', brand: 'Peugeot', color: 'bleu',
+  licensePlate: '20-AZS-15', description: 'voiture volée', listComment: [],
+  listPhoto: [], listCase: [], listTag: []},
+  {id: 2, date: '02/05/2010', name: '304', brand: 'Peugeot', color: 'bleu',
+  licensePlate: '20-AZS-15', description: 'voiture volée', listComment: [],
+  listPhoto: [], listCase: [], listTag: []},
+];
